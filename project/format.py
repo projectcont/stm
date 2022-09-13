48d090413fe3cae88e808c1d7a412dc8c17e8f93
Konstantin Bograchev <webprojcomp@gmail.com>
2022-09-13 11:57:48 +0300

    13 sept 2022

--- project/format.py
+++ project/format.py
@@ -8,7 +8,7 @@ def printf(text="",v=0):
         print(text,v)
         logs.log_txt(text,v)
 
-def printf2(text="",v=0):
+def printf2(text="", v=0):
     if v==0:
         print('           ',text)
         logs.log_txt(text,v)
